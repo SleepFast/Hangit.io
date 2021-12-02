8d26990c83372f9d58de6a2d18d93d5b6184e99b
Thomas Quinton <91420588+SleepFast@users.noreply.github.com>
2021-12-02 18:51:06 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,7 +237,7 @@ let readyPlayers = [],
 Button.openHostForm.addEventListener("click", () => {
 	invitationLink = GenerateLink();
 	current_url += `?g=${invitationLink}`;
-	Input.invitationLink.value = `https://matteoo34.github.io/hangit.io/?g=${invitationLink}`;
+	Input.invitationLink.value = `https://SleepFast/Hangit.io/?g=${invitationLink}`;
 	// Input.invitationLink.value = `http://localhost/hangit.io/?g=${invitationLink}`;
 	// Create game
 	sendData("link_game", invitationLink);
